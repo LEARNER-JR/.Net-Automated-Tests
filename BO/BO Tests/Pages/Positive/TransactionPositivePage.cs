5497c50253f45c3dcfc87de52a04a929ce2ad17e
Janerose Muthoni <Janerose.Muthoni@ngaocredit.com>
2025-09-13 11:42:03 +0300

    feat: Add Transactions Tests, Pages and Documents refactor: Place documents in the Test Ideas Folder

---
+++ BO/BO Tests/Pages/Positive/TransactionPositivePage.cs
@@ -0,0 +1,79 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+
+public class CreateTransactionPage
+{
+    private readonly IWebDriver _driver;
+
+    public CreateTransactionPage(IWebDriver driver)
+    {
+        _driver = driver;
+        PageFactory.InitElements(driver, this);
+    }
+
+    // Selectors
+    [FindsBy(How = How.XPath, Using = "//h2[text()='Create Transaction']")]
+    public IWebElement PageHeader { get; set; }
+
+    [FindsBy(How = How.Id, Using = "react-select-3-input")]
+    public IWebElement SearchUserInput { get; set; }
+
+    [FindsBy(How = How.Name, Using = "sendAmount")]
+    public IWebElement SenderAmountInput { get; set; }
+
+    [FindsBy(How = How.Name, Using = "receiverAmount")]
+    public IWebElement ReceiverAmountInput { get; set; }
+
+    [FindsBy(How = How.XPath, Using = "//button[text()='Calculate Rate']")]
+    public IWebElement CalculateRateButton { get; set; }
+
+    [FindsBy(How = How.XPath, Using = "//button[text()='Preview Transaction']")]
+    public IWebElement PreviewTransactionButton { get; set; }
+
+    [FindsBy(How = How.XPath, Using = "//input[@type='checkbox' and @name='newBeneficiary']")]
+    public IWebElement NewBeneficiaryCheckbox { get; set; }
+
+    [FindsBy(How = How.XPath, Using = "//input[@type='file' and @name='complianceDocument']")]
+    public IWebElement UploadDocumentInput { get; set; }
+
+    public void FillSenderDetails(string userName)
+    {
+        SearchUserInput.SendKeys(userName);
+    }
+
+    public void FillSenderAmount(string amount)
+    {
+        SenderAmountInput.SendKeys(amount);
+    }
+
+    public void FillReceiverAmount(string amount)
+    {
+        ReceiverAmountInput.SendKeys(amount);
+    }
+
+    public void ClickCalculateRate()
+    {
+        CalculateRateButton.Click();
+    }
+
+    public void CheckNewBeneficiaryCheckbox()
+    {
+        if (!NewBeneficiaryCheckbox.Selected)
+            NewBeneficiaryCheckbox.Click();
+    }
+
+    public void UploadSupportingDocument(string filePath)
+    {
+        UploadDocumentInput.SendKeys(filePath);
+    }
+
+    public void ClickPreviewTransaction()
+    {
+        PreviewTransactionButton.Click();
+    }
+
+    public bool IsOnCreateTransactionPage()
+    {
+        return PageHeader.Displayed;
+    }
+}
---
+++ BO/BO Tests/Tests/Positive/TransactionPositiveTests.cs
@@ -0,0 +1,69 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+[TestFixture]
+public class CreateTransactionTests
+{
+    private IWebDriver _driver;
+    private CreateTransactionPage _createTransactionPage;
+
+    [SetUp]
+    public void Setup()
+    {
+        _driver = new ChromeDriver();
+        _driver.Navigate().GoToUrl("https://sit-ui.upesimts.com/transactions/create#senderDetails");
+        _createTransactionPage = new CreateTransactionPage(_driver);
+    }
+
+    [Test]
+    public void Test_NavigateToCreateTransactionPage()
+    {
+        Assert.IsTrue(_createTransactionPage.IsOnCreateTransactionPage(), "User is not on Create Transaction page.");
+    }
+
+    [Test]
+    public void Test_FillAndSubmitTransactionForm()
+    {
+        _createTransactionPage.FillSenderDetails("John Doe");
+        _createTransactionPage.FillSenderAmount("200");
+        _createTransactionPage.FillReceiverAmount("200");
+        _createTransactionPage.ClickPreviewTransaction();
+        // Assert that the transaction summary is displayed correctly (add appropriate assertion based on the summary page)
+    }
+
+    [Test]
+    public void Test_CalculateRateUpdatesFees()
+    {
+        _createTransactionPage.FillSenderAmount("200");
+        _createTransactionPage.FillReceiverAmount("200");
+        _createTransactionPage.ClickCalculateRate();
+        // Assert that the fees and total amount are updated correctly (add appropriate assertion)
+    }
+
+    [Test]
+    public void Test_SelectCountryDropdown()
+    {
+        // Implement the country selection logic and assertions (add appropriate selector and action)
+    }
+
+    [Test]
+    public void Test_UploadSupportingDocument()
+    {
+        _createTransactionPage.UploadSupportingDocument("path/to/document.pdf");
+        // Assert that the document was uploaded successfully (add appropriate assertion)
+    }
+
+    [Test]
+    public void Test_NewBeneficiaryCheckbox()
+    {
+        _createTransactionPage.CheckNewBeneficiaryCheckbox();
+        // Assert that the associated fields are enabled (add appropriate assertion)
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        _driver.Quit();
+    }
+}
